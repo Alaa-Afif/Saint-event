36aa8fcbd6f370026c5a431cbb600fb41bbdbec5
Alaa-Afif <154674170+Alaa-Afif@users.noreply.github.com>
2024-01-04 14:50:24 +0100

    Update app.js

--- facemash/app.js
+++ facemash/app.js
@@ -3,33 +3,32 @@ let right = document.getElementById('right');
 
 let leftNum = 1;
 let rightNum = 2;
-let list = [0,1,2,3,4,5,6,7,8,9,10,11,12,13]
+var list = [1,2,3,4,5,6,7,8,9,10,11,12,13];
 
-left.addEventListener('click', function (){
-    rightNum = getimage();
-    right.setAttribute('src',`images/${rightNum}.jpeg`)
-})
-
-right.addEventListener('click', function () {
-    leftNum = getimage();
-    left.setAttribute('src',`images/${leftNum}.jpeg`)
-})
-
-
-var total = list.length;
-var number = 0;
-
-function getimage(){
+function getnumber(){
     //let number = Math.floor(Math.random() * total);
-    let number = list[Math.floor(Math.random() * total)];
-    list = [list.slice(0,number)]+[list.slice(number+1,total)];
-
-    console.log(list);
-    console.log(number);
+    var number = list[Math.floor(Math.random() * total)];
     
     if (number != leftNum && number != rightNum){
         return number;
     }else if (number == leftNum || number == rightNum){
-        return getimage();
+        return getnumber();
     }
 }
+
+left.addEventListener('click', function (){
+    rightNum = getnumber();
+    right.setAttribute('src',`images/${rightNum}.jpeg`)
+    leftNum = getnumber();
+    left.setAttribute('src',`images/${leftNum}.jpeg`)
+})
+
+right.addEventListener('click', function () {
+    rightNum = getnumber();
+    right.setAttribute('src',`images/${rightNum}.jpeg`)
+    leftNum = getnumber();
+    left.setAttribute('src',`images/${leftNum}.jpeg`)
+})
+
+var total = list.length;
+var number = 0;
